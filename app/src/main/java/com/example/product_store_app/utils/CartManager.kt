f27a6cc11c424e3ae1ab6884758a1fbfb9504ac2
Alberto Bollo <albertobollo7@protonmail.com>
2026-02-22 20:36:08 +0100

    feat: Global cart list

---
+++ app/src/main/java/com/example/product_store_app/utils/CartManager.kt
@@ -0,0 +1,7 @@
+package com.example.product_store_app.utils
+
+import com.example.product_store_app.model.Product
+// Carrito accesible desde las dos activities
+object CartManager {
+    val cartList = ArrayList<Product>()
+}
