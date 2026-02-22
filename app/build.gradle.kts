2afac86ebfc71241f3f859999d157e059e131de5
Alberto Bollo <albertobollo7@protonmail.com>
2026-02-22 20:41:48 +0100

    feat: added volley and glide dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -29,6 +29,10 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,6 +40,8 @@ android {
 }
 
 dependencies {
+    implementation(libs.glide)
+    implementation(libs.volley)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
